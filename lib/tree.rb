a6715a5516e0c104f94cd83cef1ca3e10a14a689
Jimno <menaoir.jimmy@gmail.com>
2023-03-08 16:26:00 +0100

    add visualize tree (code of TOP's student on Discord)

--- lib/tree.rb
+++ lib/tree.rb
@@ -16,9 +16,15 @@ class Tree
     node.right = build_tree(array, mid + 1, ending)
     node
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
 
 tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 
 @root = tree.build_tree(tree.array)
-p @root
+tree.pretty_print(@root)
